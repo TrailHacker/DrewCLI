111c742806eaca331b0527e5011b18b7820ae8e0
Luc <hardbarger@infoplanit.com>
2021-07-21 00:10:59 -0400

    Quick first pass at logic

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.IO;
 
-namespace Drew_CLI
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Drew_CLI {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Please enter the word you would like to replace: ");
+            var wordToFind = Console.ReadLine ();
+
+            Console.WriteLine ("Please enter the word you would like to replace it with: ");
+            var wordToReplace = Console.ReadLine ();
+
+            try {
+                using var reader = new StreamReader ("input.txt");
+                using var writer = new StreamWriter ("ouptput.txt");
+
+                string line = null;
+                while ((line = reader.ReadLine ()) != null) {
+                    line = line.Replace (wordToFind, wordToReplace);
+                    writer.WriteLine (line);
+                }
+            } catch (FileNotFoundException oopsNotFound) {
+                Console.WriteLine ("File not found! Details: " + oopsNotFound.Message);
+            }
+
         }
     }
 }
